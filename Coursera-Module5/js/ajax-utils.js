d838a6a50f0a05610341401a149ace2322747e22
Surajjha1435 <surajsambhujha@gmail.com>
2022-09-17 02:06:59 +0530

    .

--- Coursera-Module5/js/ajax-utils.js
+++ Coursera-Module5/js/ajax-utils.js
@@ -11,7 +11,7 @@ function getRequestObject() {
   } 
   else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTPS"));
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
   else {
     global.alert("Ajax is not supported!");
